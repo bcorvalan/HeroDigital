d8506ce2f4714d1c382ba02722fbbe8e1a8db616
Belén Corvalán <44319674+bcorvalan@users.noreply.github.com>
2021-08-25 13:52:25 -0300

    Update register.js

--- pages/api/register.js
+++ pages/api/register.js
@@ -3,7 +3,6 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
    
     const requestData = req.body
-    console.log(requestData)
 
     if(!requestData.triggerError) { 
       res.status(200).json({  message: "Thank you. You are now subscribed."})
